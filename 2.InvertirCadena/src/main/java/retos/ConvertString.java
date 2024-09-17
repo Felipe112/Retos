418623017b7d8ed9cd53be0bf2cc9ab1534eca22
andres ceballos <aceballos@consultec-ti.com>
2024-09-16 19:38:48 -0500

    Reto 2, pruebas

--- 2.InvertirCadena/src/main/java/retos/ConvertString.java
+++ 2.InvertirCadena/src/main/java/retos/ConvertString.java
@@ -1,5 +1,6 @@
 package retos;
 
+import java.nio.charset.StandardCharsets;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -13,7 +14,7 @@ public class ConvertString {
   ////
   //// MÉTODOS PÚBLICOS
   ////
-  
+
   /**
    * Método empleado para invertir una cadena de texto, aquí se valida que el registro no sea null o
    * vacío.
@@ -28,9 +29,12 @@ public class ConvertString {
       return StringUtils.EMPTY;
     }
 
-    StringBuilder result = new StringBuilder(request.length());
-    for (int i = request.length() - 1; i >= 0; i--) {
-      result.append(request.charAt(i));
+    String stringUtf = new String(request.getBytes(), StandardCharsets.UTF_8);
+
+    StringBuilder result = new StringBuilder(stringUtf.length());
+    for (int i = stringUtf.length() - 1; i >= 0; i--) {
+
+      result.append(stringUtf.charAt(i));
     }
 
     return result.toString();
